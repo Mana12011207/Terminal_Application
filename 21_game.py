4cdbdcecba57fe503f0f9dff466e182a24a9d7aa
mana12011207 <mana.misumi3050@gmail.com>
2023-05-07 03:33:32 +1000

    add play game function

--- 21_game.py
+++ 21_game.py
@@ -18,25 +18,6 @@ def user_turn(current_total):
         except ValueError as e:
             print(e)
 
-import random
-import csv
-from colored import fg, bg, attr
-
-
-print(f"{bg('green')}{fg('red')}Welcome to Counting to 21 Game{attr('reset')}")
-
-def user_turn(current_total):
-    valid_choices = [1, 2]
-    max_choice = 2 if current_total < 20 else 1
-    while True:
-        try:
-            choice = int(input(f"Current total: {current_total}\nYour turn, please enter 1 or 2: "))
-            if choice in valid_choices and choice <= max_choice:
-                return choice
-            else:
-                raise ValueError("Please enter 1 or 2 only: ")
-        except ValueError as e:
-            print(e)
 
 def computer_turn(current_total):
     valid_choices = [1, 2]
@@ -45,5 +26,29 @@ def computer_turn(current_total):
     print(f"Computer chose {choice}.")
     return choice
 
+
+def play_game():
+    current_total = 0
+    user_choice = None
+    while current_total < 21:
+        user_choice = user_turn(current_total)
+        current_total += user_choice
+        if current_total >= 21:
+            print(f"{bg('red')}You are the winner!{attr('reset')}")
+            with open("results.csv", "a")as results_file:
+                writer = csv.writer(results_file)
+                writer.writerow(["win"])
+            break
+        computer_choice = computer_turn(current_total)
+        current_total += computer_choice
+        if current_total >= 21:
+            print(f"{bg('blue')}You are the loser!{attr('reset')}")
+            with open("results.csv", "a")as results_file:
+                writer = csv.writer(results_file)
+                writer.writerow(["win"])
+            break
+
+if __name__ == "__main__":
+    play_game()
 
 
