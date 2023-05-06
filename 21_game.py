9faa7b814f3c50cfaae691303ced1d29a83a2936
mana12011207 <mana.misumi3050@gmail.com>
2023-05-07 03:31:46 +1000

    add computer_turn

--- 21_game.py
+++ 21_game.py
@@ -2,12 +2,21 @@ import random
 import csv
 from colored import fg, bg, attr
 
-import random
-import csv
-from colored import fg, bg, attr
-
 
 print(f"{bg('green')}{fg('red')}Welcome to Counting to 21 Game{attr('reset')}")
+
+def user_turn(current_total):
+    valid_choices = [1, 2]
+    max_choice = 2 if current_total < 20 else 1
+    while True:
+        try:
+            choice = int(input(f"Current total: {current_total}\nYour turn, please enter 1 or 2: "))
+            if choice in valid_choices and choice <= max_choice:
+                return choice
+            else:
+                raise ValueError("Please enter 1 or 2 only: ")
+        except ValueError as e:
+            print(e)
 
 import random
 import csv
@@ -28,4 +37,13 @@ def user_turn(current_total):
                 raise ValueError("Please enter 1 or 2 only: ")
         except ValueError as e:
             print(e)
+
+def computer_turn(current_total):
+    valid_choices = [1, 2]
+    max_choice = 2 if current_total < 20 else 1
+    choice = random.choice(valid_choices[:max_choice])
+    print(f"Computer chose {choice}.")
+    return choice
+
+
 
