6eb2f1398f0cfa66d9fc37c692358dfcbfafba08
mana12011207 <mana.misumi3050@gmail.com>
2023-05-07 03:29:09 +1000

    add welcome message

--- 21_game.py
+++ 21_game.py
@@ -2,3 +2,10 @@ import random
 import csv
 from colored import fg, bg, attr
 
+import random
+import csv
+from colored import fg, bg, attr
+
+
+print(f"{bg('green')}{fg('red')}Welcome to Counting to 21 Game{attr('reset')}")
+
