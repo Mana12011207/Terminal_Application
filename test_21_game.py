589c162f1f4f0c1b5f376fd83cdb135aa9834b36
mana12011207 <mana.misumi3050@gmail.com>
2023-05-07 03:40:07 +1000

    add the first test

--- test_21_game.py
+++ test_21_game.py
@@ -1,1 +1,11 @@
+import pytest
+from counting_to_21game import computer_turn, user_turn
+from unittest.mock import patch
+
+#test_user_turn_valid_input tests to ensure that the correct output is returned for a valid input (1 or 2) from the user. 
+@pytest.mark.parametrize("input_value, expected_output", [(1, 1), (2, 2)])
+def test_user_turn_valid_input(input_value, expected_output):
+    with patch('builtins.input', return_value=str(input_value)):
+        assert user_turn(5) == expected_output
+        
 
