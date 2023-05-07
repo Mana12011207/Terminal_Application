9422d6d840bb7553061488212d4e83c20f89fdd4
mana12011207 <mana.misumi3050@gmail.com>
2023-05-07 18:56:34 +0900

    add run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+if command -v python &>/dev/null; then
+    echo "Python is installed."
+else
+    echo "python is not installed."
+fi
+
+
+if [ -n "$VIRTUAL_ENV" ]; then
+    echo "Virtual environment is activated."
+else
+    echo "Virtual environment is not activated."
+fi
+
+
+
 python3 -m venv 21game-venv
 source 21game-venv/bin/activate
 pip install -r requirements.txt
